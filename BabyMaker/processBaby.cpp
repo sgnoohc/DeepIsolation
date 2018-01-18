f52307689962fc4850d3c6fe65c54afd89767597
Samuel May <sjmay@ucsd.edu>
2018-01-17 17:30:53 -0800

    Update processBaby to accept args for batch submission

--- BabyMaker/processBaby.cpp
+++ BabyMaker/processBaby.cpp
@@ -4,18 +4,23 @@
 
 int main(int argc, char* argv[])
 {
-  TString outfileid(argv[1]); 
-  TString infile(argv[2]); 
+  TString outfileid(argv[2]); 
+  TString infile(argv[1]); 
+  TString index(argv[3]);
 
   int max_events = -1;
-  if (argc >= 4) max_events = atoi(argv[3]);
+  if (argc >= 5) max_events = atoi(argv[4]);
   std::cout << "set max number of events to: " << max_events << std::endl;
   
   std::cout<<"running on file: "<<infile.Data()<<std::endl;
   
   TChain *chain = new TChain("Events");
+  TObjArray *tx = infile.Tokenize(",");
+  for (int i = 0; i < tx->GetEntries(); i++)
+    chain->Add(((TObjString *)(tx->At(i)))->String());
+
   //chain->Add(infile.Data());
-  chain->Add("/hadoop/cms/store/group/snt/run2_moriond17/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8_RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/V08-00-16/merged_ntuple_1.root");
+  //chain->Add("/hadoop/cms/store/group/snt/run2_moriond17/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8_RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/V08-00-16/merged_ntuple_1.root");
   if (chain->GetEntries() == 0) {
     std::cout << "ERROR: no entries in chain. filename was: " << infile << std::endl;
     return 2;
@@ -24,8 +29,8 @@ int main(int argc, char* argv[])
 
   char* sample;
   //MC
-  if (infile.Contains("TTJets_MassiveBinDECAY_TuneZ2star_8TeV"))     sample = Form("ttall_%s",  	 outfileid.Data());
-  else sample = Form("unknown_%s", outfileid.Data());
+  //if (infile.Contains("TTJets_MassiveBinDECAY_TuneZ2star_8TeV"))     sample = Form("ttall_%s",  	 outfileid.Data());
+  sample = Form("%s_%s", outfileid.Data(), index.Data());
 
   std::cout<<"sample is "<<sample<<std::endl;
   std::cout<<"outfileid "<<outfileid.Data()<<std::endl;
