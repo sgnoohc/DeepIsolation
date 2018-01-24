868bb490d20b010f7a57cdfc3ec0dee6eb42b77e
Samuel May <sjmay@ucsd.edu>
2018-01-24 15:28:36 -0800

    Switch from npz to hdf5 files

--- NeuralNet/learn.py
+++ NeuralNet/learn.py
@@ -1,6 +1,13 @@
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
 import keras
 import numpy
+import sys
 from sklearn import metrics
+import h5py
 
 import matplotlib
 matplotlib.use('Agg')
@@ -8,14 +15,27 @@ import matplotlib.pyplot as plt
 
 import utils
 
-npzfile = numpy.load('features.npz')
+# Parse args
+if len(sys.argv) != 3:
+  print('Incorrect number of arguments')
+  print('Arg 1: save name')
+  print('Arg 2: number of training events')
+  exit(1)
 
-global_features = npzfile['global_features']
-charged_pf_features = npzfile['charged_pf_features']
-photon_pf_features = npzfile['photon_pf_features']
-neutralHad_pf_features = npzfile['neutralHad_pf_features']
-label = npzfile['label']
-relIso = npzfile['relIso']
+savename = str(sys.argv[1])
+nTrain = int(sys.argv[2])
+
+# Read features from hdf5 file
+f = h5py.File('features.hdf5', 'r')
+
+global_features = f['global']
+charged_pf_features = f['charged_pf']
+photon_pf_features = f['photon_pf']
+neutralHad_pf_features = f['neutralHad_pf']
+label = f['label']
+relIso = f['relIso']
+
+global_features = numpy.transpose(numpy.array([relIso]))
 
 n_global_features = len(global_features[0])
 n_charged_pf_features = len(charged_pf_features[0][0])
@@ -25,6 +45,12 @@ n_neutralHad_pf_features = len(neutralHad_pf_features[0][0])
 charged_pf_timestep = len(charged_pf_features[0])
 photon_pf_timestep = len(photon_pf_features[0])
 neutralHad_pf_timestep = len(neutralHad_pf_features[0])
+
+print(n_global_features)
+print(n_charged_pf_features)
+print(n_photon_pf_features)
+print(n_neutralHad_pf_features)
+print(len(label))
 
 ################
 # Structure NN #
@@ -61,11 +87,11 @@ conv_neutralHad_pf = keras.layers.Convolution1D(3, 1, kernel_initializer = 'lecu
 # LSTMs for pf cands
 batch_momentum = 0.6
 
-lstm_charged_pf = keras.layers.LSTM(100, implementation = 2, name ='lstm_charged_pf_1')(conv_charged_pf)
+lstm_charged_pf = keras.layers.LSTM(150, implementation = 2, name ='lstm_charged_pf_1')(conv_charged_pf)
 lstm_charged_pf = keras.layers.normalization.BatchNormalization(momentum = batch_momentum, name = 'lstm_charged_pf_batchnorm')(lstm_charged_pf)
 lstm_charged_pf = keras.layers.Dropout(dropout_rate, name = 'lstm_charged_pf_dropout')(lstm_charged_pf)
 
-lstm_photon_pf = keras.layers.LSTM(50, implementation = 2, name = 'lstm_photon_pf_1')(conv_photon_pf)
+lstm_photon_pf = keras.layers.LSTM(100, implementation = 2, name = 'lstm_photon_pf_1')(conv_photon_pf)
 lstm_photon_pf = keras.layers.normalization.BatchNormalization(momentum = batch_momentum, name = 'lstm_photon_pf_batchnorm')(lstm_photon_pf)
 lstm_photon_pf = keras.layers.Dropout(dropout_rate, name = 'lstm_photon_pf_dropout')(lstm_photon_pf)
 
@@ -74,6 +100,7 @@ lstm_neutralHad_pf = keras.layers.normalization.BatchNormalization(momentum = ba
 lstm_neutralHad_pf = keras.layers.Dropout(dropout_rate, name = 'lstm_neutralHad_pf_dropout')(lstm_neutralHad_pf)
 
 # MLP to combine LSTM outputs with global features
+dropout_rate = 0.25
 merged_features = keras.layers.concatenate([lstm_charged_pf, lstm_photon_pf, lstm_neutralHad_pf, input_global])
 deep_layer = keras.layers.Dense(200, activation = 'relu', kernel_initializer = 'lecun_uniform', name = 'mlp_1')(merged_features)
 deep_layer = keras.layers.Dropout(dropout_rate, name = 'mlp_dropout_1')(deep_layer)
@@ -87,14 +114,16 @@ deep_layer = keras.layers.Dense(100, activation = 'relu', kernel_initializer = '
 deep_layer = keras.layers.Dropout(dropout_rate, name = 'mlp_dropout_5')(deep_layer)
 deep_layer = keras.layers.Dense(100, activation = 'relu', kernel_initializer = 'lecun_uniform', name = 'mlp_6')(deep_layer)
 deep_layer = keras.layers.Dropout(dropout_rate, name = 'mlp_dropout_6')(deep_layer)
+deep_layer = keras.layers.Dense(100, activation = 'relu', kernel_initializer = 'lecun_uniform', name = 'mlp_7')(deep_layer)
+deep_layer = keras.layers.Dropout(dropout_rate, name = 'mlp_dropout_7')(deep_layer)
 output = keras.layers.Dense(1, activation = 'tanh', kernel_initializer = 'lecun_uniform', name = 'output')(deep_layer)
 
 model = keras.models.Model(inputs = [input_charged_pf, input_photon_pf, input_neutralHad_pf, input_global], outputs = [output])
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy')
 
 # Train & Test
-nTrain = 400000
-nEpochs = 15
+#nTrain = 400000
+nEpochs = 3
 nBatch = 10000
 
 
@@ -103,10 +132,18 @@ prediction = model.predict([charged_pf_features[nTrain:], photon_pf_features[nTr
 
 relIso = relIso[nTrain:]*(-1)
 
+npzfile_bdt = numpy.load('bdt_roc.npz')
+fpr_bdt = npzfile_bdt['fpr']
+tpr_bdt = npzfile_bdt['tpr']
+
 fpr_re, tpr_re, thresh_re = metrics.roc_curve(label[nTrain:], relIso, pos_label = 1)
 fpr_nn, tpr_nn, thresh_nn = metrics.roc_curve(label[nTrain:], prediction, pos_label = 1)
+
+numpy.savez('ROCs/'+savename, tpr_nn=tpr_nn, fpr_nn=fpr_nn)
+
 plt.figure()
 plt.plot(fpr_re, tpr_re, color='darkred', lw=2, label='RelIso')
+plt.plot(fpr_bdt, tpr_bdt, color='aqua', lw=2, label='BDT trained w/sum. vars')
 plt.plot(fpr_nn, tpr_nn, color = 'darkorange', lw=2, label='DeepIsolation')
 plt.xscale('log')
 plt.xlim([0.001, 1.0])
@@ -116,14 +153,8 @@ plt.ylabel('True Positive Rate')
 plt.legend(loc='lower right')
 plt.savefig('plot.pdf')
 
-#value, idx = utils.find_nearest(thresh_re, 0.06)
-#print('RelIso = 0.06 cut: (%.3f, %.3f)' % (fpr_re[idx], tpr_re[idx]))
-#value_nn, idx_nn = utils.find_nearest(tpr_nn, tpr_re[idx])
-#print('Neural net same TPR: (%.3f, %.3f)' % (fpr_nn[idx_nn], tpr_nn[idx_nn]))
-
 value1, idx1 = utils.find_nearest(fpr_nn, 0.087)
 value2, idx2 = utils.find_nearest(tpr_nn, 0.817)
 
 print('Neural net FPR, TPR: (%.3f, %.3f)' % (fpr_nn[idx1], tpr_nn[idx1]))
 print('Neural net FPR, TPR: (%.3f, %.3f)' % (fpr_nn[idx2], tpr_nn[idx2]))
-
