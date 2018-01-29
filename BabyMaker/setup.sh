a6daf136685fa912979d47c8881386307bc03b1c
Philip Chang <sgnoohc@gmail.com>
2018-01-29 11:27:34 -0800

    update

--- BabyMaker/setup.sh
+++ BabyMaker/setup.sh
@@ -1,6 +1,6 @@
 source /code/osgcode/cmssoft/cmsset_default.sh > /dev/null 2>&1
-export SCRAM_ARCH=slc6_amd64_gcc530   # or whatever scram_arch you need for your desired CMSSW release
-export CMSSW_VERSION=CMSSW_9_2_0
+export SCRAM_ARCH=slc6_amd64_gcc630   # or whatever scram_arch you need for your desired CMSSW release
+export CMSSW_VERSION=CMSSW_9_3_0
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
 eval `scramv1 runtime -sh`
