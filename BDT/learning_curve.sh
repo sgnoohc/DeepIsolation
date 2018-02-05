d3aa05b456aed0c22244b1661db95832aea8bf06
Samuel May <sjmay@ucsd.edu>
2018-02-04 17:46:53 -0800

    Add script for training with various number of events

---
+++ BDT/learning_curve.sh
@@ -0,0 +1,6 @@
+root -l -b -q "learn.C+(10000)" > results_10k.txt
+root -l -b -q "learn.C+(33333)" > results_33k.txt
+root -l -b -q "learn.C+(100000)" > results_100k.txt
+root -l -b -q "learn.C+(333333)" > results_333k.txt
+root -l -b -q "learn.C+(1000000)" > results_1m.txt
+root -l -b -q "learn.C+(3333333)" > results_3m.txt
