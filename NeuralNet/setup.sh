bfb0da47026b51f5e23f01f9eb15e5462f1717c6
Samuel May <sjmay@ucsd.edu>
2018-04-17 06:31:42 -0700

    Add setup script for latest version of sci-kit learn

--- NeuralNet/setup.sh
+++ NeuralNet/setup.sh
@@ -1,5 +1,5 @@
 cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_9_2_8/
 cmsenv
 cd -
-pip install --user -U scikit-learn
+#pip install --user -U scikit-learn
 export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages/:$PYTHONPATH
